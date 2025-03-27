7c0456f0f4e19705cd2348fc52d0edb19442cfcb
VR <101288516+VillainsRule@users.noreply.github.com>
2025-03-27 11:02:30 -0400

    adjust discord link

--- src/console/script.js
+++ src/console/script.js
@@ -102,7 +102,7 @@ let commands = {
             '    CrackedShell is made possible by the work of one person.',
             '    If you like the project, please consider power poles.',
             '      Source: \x1b[34;1mhttps://github.com/VillainsRule/CrackedShell\x1b[0m',
-            '      Discord: \x1b[34;1mhttps://dsc.gg/sfnetwork\x1b[0m',
+            '      Discord: \x1b[34;1mhttps://discord.getstate.farm\x1b[0m',
             '      Bug Report: \x1b[34;1mhttps://github.com/VillainsRule/CrackedShell/issues/new\x1b[0m',
             ''
         ].join('\n\r'));
